52ea9c7f3c4a5b03cc494b6b5779170ea42e3da7
Dharmdip Rathod <dharmdip.rathod@gmail.com>
2017-02-23 15:29:59 +0530

    Remove addressable gem

--- lib/npiregistry/request.rb
+++ lib/npiregistry/request.rb
@@ -1,6 +1,5 @@
 require 'net/http'
 require 'json'
-require 'addressable/uri'
 require 'ostruct'
 
 module Npiregistry
@@ -15,41 +14,37 @@ module Npiregistry
 
     def initialize args
       @options = (args[:options] || {}).dup
-      @url = URL
     end
 
     def execute
-       return 'Please make sure there are enough parameters to make request.' if @options.empty?
-       make_request
+      return 'Please make sure there are enough parameters to make request.' if @options.empty?
+      make_request
     end
 
     def process_parameters
       options = OpenStruct.new(@options)
       {
-        number: options.number,
-        enumeration_type: options.enumeration_type,
-        taxonomy_description: options.taxonomy_description,
-        first_name: options.first_name,
-        last_name: options.last_name,
-        organization_name: options.organization_name,
-        address_purpose: options.address_purpose,
-        city: options.city,
-        state: options.state,
-        postal_code: options.postal_code,
-        country_code: options.country_code,
-        limit: options.limit,
-        skip: options.skip
+          number: options.number,
+          enumeration_type: options.enumeration_type,
+          taxonomy_description: options.taxonomy_description,
+          first_name: options.first_name,
+          last_name: options.last_name,
+          organization_name: options.organization_name,
+          address_purpose: options.address_purpose,
+          city: options.city,
+          state: options.state,
+          postal_code: options.postal_code,
+          country_code: options.country_code,
+          limit: options.limit,
+          skip: options.skip
       }.delete_if { |key, val| val.nil? }
     end
 
     def make_request
-      JSON.parse(::Net::HTTP.get(URI("#{@url}?#{prepare_params}")))
-    end
-
-    def prepare_params
-      uri = Addressable::URI.new
-      uri.query_values = process_parameters
-      uri.query
+      uri = URI URL
+      uri.query = URI.encode_www_form process_parameters
+      uri.header = default_headers
+      JSON.parse Net::HTTP.get_response(uri).body
     end
 
     def default_headers
